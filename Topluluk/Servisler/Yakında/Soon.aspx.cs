b33ba50a2f9778eae32110a1d8e53f645eb47b38
Arda <47920304+ardatdev@users.noreply.github.com>
2021-05-06 15:11:47 +0300

    Add files via upload

---
+++ Topluluk/Servisler/Yakında/Soon.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Topluluk_Servisler_Yakında_Soon : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
