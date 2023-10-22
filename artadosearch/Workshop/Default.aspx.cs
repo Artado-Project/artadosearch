c70c414cb73bf71740d904af6173f50b360a491e
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-10-22 15:03:48 +0300

    "Defaukt" and "ProductPPage" typos have been corrected as "Default" "ProductPage".

--- artadosearch/Workshop/Default.aspx.cs
+++ artadosearch/Workshop/Default.aspx.cs
@@ -8,7 +8,7 @@ using System.Web.UI.WebControls;
 
 namespace artadosearch.Workshop
 {
-    public partial class Defaukt : System.Web.UI.Page
+    public partial class Default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
--- artadosearch/Workshop/ProductPage.aspx.cs
+++ artadosearch/Workshop/ProductPage.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace artadosearch.Workshop
 {
-    public partial class ProductPPage : System.Web.UI.Page
+    public partial class ProductPage : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
