04f0f43c58ae8157685e3446767d6a09ec5d811e
Yasin Özkaya <90653146+yasinldev@users.noreply.github.com>
2023-12-23 16:13:50 +0300

    Update: App file contents linked to router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import ArtadoHeader from "./components/Index/ArtadoHeader";
-import ArtadoFooter from "./components/Index/ArtadoFooter";
-import ArtadoSearchBar from "./components/Index/ArtadoSearchBar";
-import ArtadoSection from "./components/Index/ArtadoSection";
-
-import './assets/Index.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from './pages/index';
+import Search from './pages/search';
 
 function App() {
   return (
-    <div className="Artado-container is-fluid" style={{ margin: "10px" }}>
-      <ArtadoHeader />
-        <div style={{marginTop: '100px'}}>
-            <ArtadoSearchBar/>
-        </div>
-        <ArtadoSection />
-        <ArtadoFooter />
-    </div>
+      <>
+          <Router>
+              <Routes>
+                  <Route path={'/'} element={<Home />} />
+                  <Route path={'/search'} element={<Search />} />
+              </Routes>
+          </Router>
+      </>
   );
 }
 
