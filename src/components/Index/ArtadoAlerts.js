9de6f8f9f8cfe0d702d7321bf945bf43e81d2fad
Yasin Özkaya <90653146+yasinldev@users.noreply.github.com>
2023-12-23 20:56:02 +0300

    Update: Added Alerts for Artado Index

---
+++ src/components/Index/ArtadoAlerts.js
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import { message } from 'antd';
+
+const AlertMessage = () => {
+    useEffect(() => {
+        const alertParams = new URLSearchParams(window.location.search);
+        const alertType = alertParams.get('ret_no');
+
+        const showAlert = () => {
+            switch (alertType) {
+                case 'empty-url':
+                    message.warning({
+                        content: 'Attention! Input value cannot be empty! (EMPTY-URL)',
+                        style: {
+                            fontWeight: 'bold',
+                            fontSize: '0.8rem',
+                            fontFamily: 'assistant',
+                        }
+                    });
+                    break;
+
+                default:
+                    break;
+            }
+        };
+
+        showAlert();
+    }, );
+
+    return null;
+};
+
+export default AlertMessage;
