ca93fdd39b875e64f298011907a4dbdae0d80832
Yasin Özkaya <90653146+yasinldev@users.noreply.github.com>
2023-12-23 20:57:04 +0300

    Update: Imported SearchHeader

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,1 +1,13 @@
-// ...
+import SearchHeader from './../components/Search/SearchHeader';
+
+import './../assets/Index.css';
+
+function Search() {
+  return (
+    <div className={'Artado-container is-fluid'} style={{ margin: '10px' }}>
+      <SearchHeader />
+    </div>
+  );
+}
+
+export default Search;
