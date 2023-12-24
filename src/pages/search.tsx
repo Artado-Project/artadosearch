0aaa8acc688995b94dc312f4915caeb8fbc43c5a
Yasin Özkaya <90653146+yasinldev@users.noreply.github.com>
2023-12-24 23:53:07 +0300

    Update: Responsive

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchHeader from '../components/Search/SearchHeader';
+import SearchWiki from "../components/Search/SearchWiki";
 
 import './../assets/Index.css';
 
@@ -7,6 +8,13 @@ function Search() {
   return (
     <div className={'Artado-container is-fluid'} style={{ margin: '10px' }}>
       <SearchHeader />
+      <div className={'Artado-row'} style={{ marginTop: 50 }}>
+          <div className={'column-xs-12 column-sm-12 column-md-8 column-lg-8 column-xl-8'}>
+          </div>
+          <div className={'column-xs-12 column-sm-12 column-md-4 column-lg-4 column-xl-4'}>
+              <SearchWiki />
+          </div>
+      </div>
     </div>
   );
 }
