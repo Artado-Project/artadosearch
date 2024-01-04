45f37228621701b775c36a7adf7a298f65e39ab6
Yasin Özkaya <90653146+yasinldev@users.noreply.github.com>
2024-01-04 19:53:58 +0300

    Update: Added SearchResult.tsx file and some improvements

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchHeader from '../components/Search/SearchHeader';
 import SearchWiki from "../components/Search/SearchWiki";
+import SearchResult from "../components/Search/SearchResult";
 
 import './../assets/Index.css';
 
@@ -8,9 +9,11 @@ function Search() {
   return (
     <div className={'Artado-container is-fluid'} style={{ margin: '10px' }}>
       <SearchHeader />
-      <div className={'Artado-row'} style={{ marginTop: 50 }}>
-          <div className={'column-xs-12 column-sm-12 column-md-8 column-lg-8 column-xl-8'}>
+      <div className={'Artado-row'} style={{ marginTop: 15 }}>
+          <div className={'column-xs-12 column-sm-12 column-md-7 column-lg-7 column-xl-7'}>
+              <SearchResult />
           </div>
+          <div className={'column-xs-12 column-sm-12 column-md-1 column-lg-1 column-xl-1'}></div>
           <div className={'column-xs-12 column-sm-12 column-md-4 column-lg-4 column-xl-4'}>
               <SearchWiki />
           </div>
