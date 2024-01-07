896a7182aaceaa4bc79f8be4ac39e18a0389760a
Yasin Özkaya <90653146+yasinldev@users.noreply.github.com>
2024-01-07 17:10:28 +0300

    Update: Improvements again

--- src/components/Search/SearchResult.tsx
+++ src/components/Search/SearchResult.tsx
@@ -56,7 +56,7 @@ const SearchResult: React.FC = () => {
                 title={
                     <>
                         <span>Turkey - Wikipedia</span> <br />
-                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
                             <small style={{ color: '#7c7c7c' }}>en.wikipedia.org  › wiki › Turkey</small>
                             <Tag color={'green'}>Encyclopedia ~ Verified</Tag>
                         </div>
@@ -88,7 +88,7 @@ const SearchResult: React.FC = () => {
                             d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286m1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94"/>
                     </svg>
                 </div>
-                <h3 className={'result-title'}>Turkey - Wikipedia</h3>
+                <a href={'#'} style={{ cursor: 'pointer' }} className={'result-title'}>Turkey - Wikipedia</a>
                 <div className={'result-url'}>en.wikipedia.org  › wiki › Turkey</div> <br />
                 <span className={'result-desc'}>
                     <Tag color={'#a1a1a1'}>Encyclopedia</Tag> Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consequatur consequuntur culpa deserunt doloremque eligendi illo illum ipsa laboriosam laudantium nesciunt nostrum, officiis quibusdam quos sequi sunt, totam vitae voluptate.
