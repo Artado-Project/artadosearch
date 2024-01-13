8c9c87270b5a9e0a3f693395862b1d77c3a3ac06
yasinldev <ozkayayasin964@gmail.com>
2024-01-13 21:37:53 +0300

    Update: Improvements

--- src/components/Search/SearchWiki.tsx
+++ src/components/Search/SearchWiki.tsx
@@ -53,8 +53,8 @@ const SearchWiki: React.FC = () => {
                    <span style={{ fontSize: '14px', lineHeight: '24px' ,color: '#5c5c5c' }}>
                         The Republic of Turkey, is a transcontinental country located mainly on the Anatolian Peninsula in Western Asia, with a smaller portion on the Balkan Peninsula in Southeastern Europe. Turkey is bordered on its northwest by Greece and Bulgaria; north by the Black Sea; northeast by Georgia; east by Armenia, Azerbaijan, and Iran; southeast by Iraq; south by Syria and the Mediterranean Sea; and west by the Aegean Sea. Approximately 70 to 80 percent of the country's citizens identify as Turkish, while Kurds are the largest minority, at between 15 to 20 percent of the population.
                    </span>
-                   <Divider orientation={'center'} plain={true} style={{fontSize: '14px' }}>More Information</Divider>
-                   <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+                   <Divider orientation={'center'} plain={true} style={{fontSize: '13px', fontWeight: 'bold', color: '#7c7c7c', fontFamily: 'assistant, sans-serif' }}>More Information</Divider>
+                   <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between', marginTop: '5px' }}>
                        {about.map((item, index) => {
                            return (
                                <div key={index} style={{ flexBasis: '48%', marginBottom: '10px' }}>
