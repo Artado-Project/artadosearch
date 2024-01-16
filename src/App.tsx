8ff5033b6842abae2348d893d26285225e25de0c
yasinldev <ozkayayasin964@gmail.com>
2024-01-16 03:29:49 +0300

    Added: Manifest file

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from './pages';
 import Search from './pages/search';
+import Manifest from './pages/manifest';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
               <Routes>
                   <Route path={'/'} element={<Home />} />
                   <Route path={'/search'} element={<Search />} />
+                  <Route path={'/manifest'} element={<Manifest/>} />
               </Routes>
           </Router>
       </>
---
+++ src/pages/manifest.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import './../assets/Index.css';
+import { Button } from "antd";
+
+function Manifest() {
+    return (
+        <>
+            <div className={'Artado-container'} style={{ lineHeight: '25px' }}>
+                <h1 className="manifest-title">Artado Manifest </h1>
+                <Button type="default" size={'small'} href={'/'}>Back to Home</Button>
+                <br />
+                <br />
+                Today, the internet is the greatest tool for communication and information exchange. Thanks to the internet, we can meet new people, learn new information, and share our experiences with others. The internet is one of the greatest inventions of the human race. It makes our lives easier. Unfortunately, not everyone can access this great invention freely. We want the internet to be accessible to everyone, without restrictions from any power or authority. Accessing the wealth of information on the internet is a fundamental right.
+                <br /> <br />
+                We advocate for an internet where everyone can engage in free information exchange, where personal data is respected, and where ideas can be expressed without censorship.
+                <br /><br />
+                <h2 className="manifest-subtitle">Why do we care so much about a free internet?</h2>
+                People can share anything quickly and effectively through the internet. The internet allows rapid communication with millions. It is a tool for amplifying the voice of the public. Therefore, the internet should be accessible to everyone freely.
+                <br /><br />
+                Freedom of thought is a cornerstone of democracy. Restricting freedom of thought in democratically governed countries is a betrayal of democracy and the people. Freedom of thought should be a right for all of humanity, not just in democratic countries. The internet serves as a means to fulfill this right. Restricting this tool is a denial of one's fundamental rights.
+                <br /><br/>
+                <h2 className="manifest-subtitle">Why you should not pay for search?</h2>
+                Search engines are the most used websites on the internet. They are the first step to accessing the internet. Therefore, search engines should be free and accessible to everyone. Search engines should not be a tool for profit. Search engines should be a tool for the people.
+                <br /><br />
+                Charging people a price for access to accurate information is anathema to democracy. Turning search engines into a revenue stream and charging money for accurate information or otherwise throwing them into misleading information pollution (advertisements) is treason in the Internet country. We think that we should direct all people to the right information, especially in this period of information pollution, and that this is a human duty, and we defend this idea in a widespread way.
+                <br /><br />
+                <h2 className='manifest-subtitle'>Embracing anti-censorship</h2>
+                Suppression of information is a mechanism employed by authoritarian regimes to curtail the unfettered exploration of ideas and perspectives. It becomes a favored tool for those who harbor apprehensions about confronting the unfiltered reality, aiming to shield certain narratives from public scrutiny. This tactic is often wielded by individuals or entities with an inclination to withhold the complete truth, thereby fostering an environment where transparency and open discourse are sacrificed in favor of a controlled narrative.
+                <br /><br />
+                The internet should be accessible to everyone, without restrictions from any power or authority. Accessing the wealth of information on the internet is a fundamental right. We advocate for an internet where everyone can engage in free information exchange, where personal data is respected, and where ideas can be expressed without censorship.
+                <br /><br />
+                <h2 className="manifest-subtitle">Anonymity</h2>
+                One of the most important aspects the internet brings is anonymity, but it can also be abused. The solution is not to restrict the internet or eliminate anonymity entirely. The fundamental and definitive solution is education and justice.
+                <br /><br />
+                An educated individual, enjoying freedom of thought, doesn't require anonymity. They know that expressing their ideas properly won't lead to negative consequences, and they won't misuse anonymity due to their education. Individuals who express themselves anonymously are often those without freedom of thought. An individual expresses ideas anonymously due to the fear of potential harm. Authorities seeking to prevent this misuse often attempt to eliminate anonymity or restrict the internet.
+                <br /><br />
+                Anonymous identities provided by the internet grant freedom of thought to those who lack it.
+                <br /><br />
+                <h2 className="manifest-subtitle">Right to Privacy</h2>
+                Privacy is one of the fundamental rights of individuals. Personal data should not be collected via the internet tools people use. Personal data is private and taking it without consent is a violation. At Artado, we respect individuals' personal data and strive to navigate the internet freely while protecting personal data. Individuals have the responsibility to choose the right tools to safeguard their personal data.
+                <br /><br />
+                <h2 className="manifest-subtitle">Making the Internet a Better Place</h2>
+                Implementing the principles above will truly make the internet a beautiful place. In the 21st century, the internet is both an entertainment network and a communication network. It is one of the most important factors for human progress. We at Artado support the principles mentioned above and invite you to do the same. The internet will be a better place when we unite!
+                <br /><br />
+            </div>
+        </>
+    )
+}
+
+export default Manifest;
