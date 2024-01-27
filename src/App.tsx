fc73c1182a899d4e90440a6981a111bf570bc54d
yasinldev <ozkayayasin964@gmail.com>
2024-01-27 16:40:50 +0300

    Update: Added settings page link

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from './pages';
 import Search from './pages/search';
 import Manifest from './pages/manifest';
+import Settings from './pages/settings';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
                   <Route path={'/'} element={<Home />} />
                   <Route path={'/search'} element={<Search />} />
                   <Route path={'/manifest'} element={<Manifest/>} />
+                  <Route path={'/settings'} element={<Settings />} />
               </Routes>
           </Router>
       </>
