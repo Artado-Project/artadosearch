de30b7ad5e9eabb2c7223360324e7a44929ca3d0
yasinldev <ozkayayasin964@gmail.com>
2024-04-15 23:19:17 +0300

    Update: Added mic button for voice search

--- src/components/Index/ArtadoSearchBar.tsx
+++ src/components/Index/ArtadoSearchBar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react';
 import './../../assets/Index.css';
-import {AutoComplete} from 'antd';
+import {AutoComplete, Button} from 'antd';
 
 const language = localStorage.getItem('language') ?? 'en';
 const languageData = require(`./../../language/${language}/index-page.json`);
@@ -10,8 +10,42 @@ const mockVal = (str: string, repeat = 1) => ({
 });
 
 const getInputClassName = () => {
-    const input = document.getElementsByClassName('ant-select-selector');
+    const input: HTMLCollectionOf<Element> = document.getElementsByClassName('ant-select-selector');
     input.item(0)?.setAttribute('style', 'border-radius: 60px !important');
+}
+
+declare global {
+    interface Window {
+        SpeechRecognition: any,
+        webkitSpeechRecognition: any,
+        mozSpeechRecognition: any,
+        msSpeechRecognition: any,
+    }
+}
+
+const recognition = () => {
+    const start = document.getElementById('speech');
+    const out = document.getElementById('searchInput');
+
+    const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+    recognition.lang = 'en-US';
+
+    recognition.onstart = () => {
+        console.log('listening...');
+    };
+
+    recognition.onresult = (event: any) => {
+        const transcript = event.results[0][0].transcript;
+        out!.textContent = transcript;
+    };
+
+    recognition.onend = () => {
+        console.log('listening end');
+    };
+
+    start!.addEventListener('click', () => {
+        recognition.start();
+    });
 }
 
 const ArtadoSearchBar: React.FC = () => {
@@ -104,15 +138,30 @@ const ArtadoSearchBar: React.FC = () => {
                         onSelect={onSelect}
                         onSearch={(text) => setOptions(getPanelValue(text))}
                         placeholder={languageData.input}
-                        suffixIcon={
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                 className="bi bi-search-heart" viewBox="0 0 16 16">
-                                <path d="M6.5 4.482c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018"/>
-                                <path
-                                    d="M13 6.5a6.47 6.47 0 0 1-1.258 3.844q.06.044.115.098l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1-.1-.115h.002A6.5 6.5 0 1 1 13 6.5M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11"/>
-                            </svg>
-                        }
                     />
+                    <Button
+                        type="default"
+                        shape="circle"
+                        id={'speech'}
+                        size={'middle'}
+                        style={{
+                            marginTop: 10,
+                            display: "flex",
+                            textAlign: "center",
+                            alignItems: "center",
+                            alignContent: "center",
+                            justifyContent: "center",
+                            color: '#8c8c8c',
+                        }}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"
+                             className="bi bi-mic" viewBox="0 0 16 16">
+                            <path
+                                d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5"/>
+                            <path
+                                d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3"/>
+                        </svg>
+                    </Button>
                     <input
                         id={'searchInput'}
                         style={{borderRadius: '60px !important'}}
