c51ef2abc65b8f0147652f21d97f9fd7930d0f6e
yasinldev <ozkayayasin964@gmail.com>
2024-05-02 05:42:23 +0300

    Removed: The More Questions section was deleted because artado search doesn't follow you, so it can't know what users are searching for more.

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import SearchHeader from '../components/Search/SearchHeader';
 import SearchWiki from "../components/Search/SearchWiki";
 import SearchResult from "../components/Search/SearchResult";
-import SearchQuestions from "../components/Search/SearchQuestions";
 
 import './../assets/Index.css';
 
@@ -13,7 +12,6 @@ function Search() {
       <div className={'Artado-row'} style={{ marginTop: 15 }}>
           <div className={'column-xs-12 column-sm-12 column-md-7 column-lg-7 column-xl-7'} id={'responsive-end'}>
               <SearchResult />
-              <SearchQuestions />
           </div>
           <div className={'column-xs-12 column-sm-12 column-md-1 column-lg-1 column-xl-1'} id={'responsive-none'}></div>
           <div className={'column-xs-12 column-sm-12 column-md-4 column-lg-4 column-xl-4'} id={'responsive-first'}>
