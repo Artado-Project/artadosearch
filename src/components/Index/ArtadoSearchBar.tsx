69f9cd6b516d3087a1efd6b5f7ecc82d23d2a2e0
yasinldev <ozkayayasin964@gmail.com>
2024-05-23 05:14:06 +0300

    Update: removed ads section from index page

--- src/components/Index/ArtadoSearchBar.tsx
+++ src/components/Index/ArtadoSearchBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react';
-import {AutoComplete, Button} from 'antd';
+import {AutoComplete} from 'antd';
 
 const language = localStorage.getItem('language') ?? 'en';
 const languageData = require(`./../../language/${language}/index-page.json`);
@@ -138,29 +138,6 @@ const ArtadoSearchBar: React.FC = () => {
                         onSearch={(text) => setOptions(getPanelValue(text))}
                         placeholder={languageData.input}
                     />
-                    <Button
-                        type="default"
-                        shape="circle"
-                        id={'speech'}
-                        size={'middle'}
-                        style={{
-                            marginTop: 10,
-                            display: "flex",
-                            textAlign: "center",
-                            alignItems: "center",
-                            alignContent: "center",
-                            justifyContent: "center",
-                            color: '#8c8c8c',
-                        }}
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"
-                             className="bi bi-mic" viewBox="0 0 16 16">
-                            <path
-                                d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5"/>
-                            <path
-                                d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3"/>
-                        </svg>
-                    </Button>
                     <input
                         id={'searchInput'}
                         style={{borderRadius: '60px !important'}}
